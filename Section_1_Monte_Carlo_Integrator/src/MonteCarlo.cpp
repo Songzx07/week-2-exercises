b19241533e43cb7dca70c106bb004592dea3ddb9
Zhexuan <ucapzso@ucl.ac.uk>
2025-01-24 15:29:56 +0000

    Implement Exercise of Section 1 of Week 2

--- Section_1_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_1_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -1,39 +1,57 @@
 #include <iostream>
 #include <cmath>
+#include <random>
+#include <functional>
 
-bool InsideUnitSphere(double x, double y, double z)
+using std::bind;
+using std::cout;
+using std::function;
+using std::mt19937;
+using std::stoi;
+using std::uniform_real_distribution;
+
+function<bool(double, double, double)> InsideUnitSphere = [](double x, double y, double z)
+{ return ((x * x + y * y + z * z) <= 1); };
+
+double IntegrateMonteCarlo3D(int n_points, double min, double max, int seed, function<bool(double, double, double)> InsideUnitSphere)
 {
-    return ((x*x + y*y + z*z) <= 1);
+  int count = 0;
+  // N.B. std::pow with integer arguments can be less efficient than
+  // writing the multiplication explicitly, so be careful about using it
+  // inside loops for example!
+  double VolCube = std::pow((max - min), 3);
+
+  // set up random number generator here
+  uniform_real_distribution<double> uniform_dist(min, max);
+  mt19937 rng_mt(seed);
+  auto uni_rng = bind(uniform_dist, rng_mt);
+
+  for (int i = 0; i < n_points; i++)
+  {
+    // generate random points here
+    double x = uni_rng();
+    double y = uni_rng();
+    double z = uni_rng();
+    if (InsideUnitSphere(x, y, z))
+      count++;
+  }
+
+  return static_cast<double>(count) / n_points * VolCube;
 }
 
-double IntegrateMonteCarlo3D(int n_points, double min, double max)
+int main(int argc, char **argv)
 {
-    int count = 0;
-    // N.B. std::pow with integer arguments can be less efficient than
-    // writing the multiplication explicitly, so be careful about using it
-    // inside loops for example!
-    double VolCube = std::pow((max - min), 3);
+  if (argc != 2)
+  {
+    cout << "Please provide the seed as an argument to the program" << "\n";
+    return 1;
+  }
 
-    //set up random number generator here
+  int seed = stoi(argv[1]);
 
-    for(int i = 0; i < n_points; i++)
-    {
-        //generate random points here
-        double x = 
-        double y =
-        double z = 
-        if(InsideUnitSphere(x, y, z)) count++;
-    }
+  int N_points = 10000;
 
-    return static_cast<double>(count) / n_points * VolCube;
-}
+  double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0, seed, InsideUnitSphere);
 
-
-int main()
-{
-    int N_points = 10000;
-    
-    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0);
-
-    std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
+  std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
 }
