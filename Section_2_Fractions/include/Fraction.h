1e4135914e13bc9a6fe8a1708ea051ddae7f9752
Zhexuan <ucapzso@ucl.ac.uk>
2025-01-30 10:09:26 +0000

    Implement Fraction class with basic operations and simplification

--- Section_2_Fractions/include/Fraction.h
+++ Section_2_Fractions/include/Fraction.h
@@ -1,2 +1,27 @@
 #pragma once
 #include <string>
+
+class Fraction
+{
+public:
+  Fraction(int a, int b);
+
+  ~Fraction();
+
+  Fraction recripocal();
+
+  Fraction multipliy(int a);
+
+  double toDouble();
+
+  std::string toString();
+
+  int getNumerator();
+
+  int getDenominator();
+
+private:
+  int numerator;
+  int denominator;
+  void simplify();
+};
--- Section_2_Fractions/src/Fraction.cpp
+++ Section_2_Fractions/src/Fraction.cpp
@@ -1,1 +1,61 @@
 #include "Fraction.h"
+
+Fraction::Fraction(int a, int b)
+{
+  numerator = a;
+  denominator = b;
+
+  if (denominator == 0)
+  {
+    throw "Denominator cannot be zero";
+  }
+
+  simplify();
+}
+
+Fraction::~Fraction() {}
+
+void Fraction::simplify()
+{
+  int gcd = 1;
+  for (int i = 1; i <= numerator && i <= denominator; i++)
+  {
+    if (numerator % i == 0 && denominator % i == 0)
+    {
+      gcd = i;
+    }
+  }
+  numerator /= gcd;
+  denominator /= gcd;
+}
+
+Fraction Fraction::recripocal()
+{
+  return Fraction(denominator, numerator);
+}
+
+Fraction Fraction::multipliy(int a)
+{
+  return Fraction(numerator * a, denominator);
+}
+
+double Fraction::toDouble()
+{
+  return (double)numerator / denominator;
+}
+
+std::string Fraction::toString()
+{
+  if (numerator == 0)
+  {
+    return "0";
+  }
+  else if (denominator == 1)
+  {
+    return std::to_string(numerator);
+  }
+  else
+  {
+    return std::to_string(numerator) + "/" + std::to_string(denominator);
+  }
+}
